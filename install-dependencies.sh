3debb1e43e533e04522646445c4d04dabafefe56
Evan <ehutch7@gmail.com>
2023-11-16 15:23:34 +0000

    Adding some colors to the stdout and a gitignore file

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+
+COLOR_OFF="\e[0m"
+GREEN="\e[1;32m"
+YELLOW="\e[1;33m"
+CYAN="\e[1;36m"
 #
 # TODO
 # Check if deps are not only present (-f), but executable (-x)
 # if present and not executable, run a `chmod` to fix it
 
+echo -e "${GREEN}Let's get crackin'!${COLOR_OFF}"
+
 # Install Homebrew/Linuxbrew
 # kernel_name=$(uname -s) # <-- not sure if we need custom config for Linux vs MacOS
 # kernal_name == 'Darwin' || 'Linux'
 if [[ ! -f "$(brew --prefix)/bin/brew" ]]; then
-    echo "Installing Homebrew..."
+    echo -e "${CYAN}Installing Homebrew...${COLOR_OFF}"
     zsh $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
 fi
 
@@ -16,7 +23,7 @@ fi
 # Install Brew Leaves
 script_path="$(dirname $0)/brew-leaves.txt"
 if [[ -e $script_path && -s $script_path ]]; then
-  echo "Installing Brew Leaves..."
+  echo -e "${CYAN}Installing Brew Leaves...${COLOR_OFF}"
   xargs brew install < "brew-leaves.txt"
 else
   echo "No brew leaves to install."
@@ -24,7 +31,7 @@ fi
 
 # Install zap-zsh
 if [[ ! -f "$HOME/.local/share/zap/zap.zsh" ]]; then
-    echo "Installing Zap-Zsh..."
+    echo -e "${CYAN}Installing Zap-Zsh...${COLOR_OFF}"
     zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
 fi
 
@@ -36,7 +43,7 @@ fi
 
 # LunarVim
 if [[ ! -f "$HOME/.local/bin/lvim" ]]; then
-  echo "Installing LunarVim..."
+  echo -e "${CYAN}Installing LunarVim...${COLOR_OFF}"
   ## Might need to check and install lvim deps like cargo, node, make, etc.
   ## Also need to remove the default lvim user config file and use stowed version.
   LV_BRANCH='release-1.3/neovim-0.9' zsh <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
@@ -45,12 +52,16 @@ fi
 # Volta
 
 if [[ ! -f "$HOME/.volta/bin/volta" ]]; then
-  echo "Volta is not installed"
-  read -q "?Would you like to install it? (y/n): " answer
+  echo -e "${YELLOW}Volta is not installed${COLOR_OFF}"
+  echo -e -n "${YELLOW}Would you like to install it? ${GREEN}(y/n):${COLOR_OFF} "   
+  read -q answer 
+  echo
   if [[ $answer == "Y" || $answer == "y" ]]; then
-    echo "Installing Volta..."
+    echo -e "${CYAN}Installing Volta...${COLOR_OFF}"
     zsh <(curl https://get.volta.sh)
   else
-    echo "Volta will not be installed."
+    echo -e "${CYAN}Volta will not be installed.${COLOR_OFF}"
   fi
 fi
+
+echo -e "${GREEN}All Done!${COLOR_OFF}"
