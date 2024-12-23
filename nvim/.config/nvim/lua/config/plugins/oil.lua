1bbd14e31371750a893f81fe76a8430b1fafa95a
Evan <ehutch7@gmail.com>
2024-12-23 04:11:31 +0000

    Adjusting some mininvim, oil, and telescope configs

--- nvim/.config/nvim/lua/config/plugins/oil.lua
+++ nvim/.config/nvim/lua/config/plugins/oil.lua
@@ -1,7 +1,11 @@
 return {
 	{
 		'stevearc/oil.nvim',
-		opts = {},
+		opts = {
+			view_options = {
+				show_hidden = true
+			}
+		},
 		-- Optional dependencies
 		dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	}
--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -11,8 +11,26 @@ return {
 	},
 	config = function()
 		require("telescope").setup({
+			defaults = {
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"--hidden",
+					"--glob=!.git/",
+				},
+			},
 			extensions = {
 				fzf = {}
+			},
+			pickers = {
+				find_files = {
+					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" }
+				}
 			}
 		})
 
