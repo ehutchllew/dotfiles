b2def0caace837eb9e4514588625dc5a19a67592
Evan <ehutch7@gmail.com>
2025-01-01 04:05:06 +0000

    Improving zsh exports file to conditionally add things to path; improving install_deps shell script to output more to stdout and include MISE installation.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -17,26 +17,36 @@ echo -e "${GREEN}Let's get crackin'!${COLOR_OFF}"
 if [[ ! -f "$(brew --prefix)/bin/brew" ]]; then
     echo -e "${CYAN}Installing Homebrew...${COLOR_OFF}"
     zsh $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
+    echo -e "${GREEN}Homebrew Installed!${COLOR_OFF}"
 fi
 
 ## TODO: Depecreate brew leaves in favor of the mise `config.toml`
 # Install Brew Leaves
 script_path="$(dirname $0)/brew-leaves.txt"
 if [[ -e $script_path && -s $script_path ]]; then
-  echo -e "${CYAN}Installing Brew Leaves...${COLOR_OFF}"
-  xargs brew install < "brew-leaves.txt"
+  	echo -e "${CYAN}Installing Brew Leaves...${COLOR_OFF}"
+  	xargs brew install < "brew-leaves.txt"
+  	echo -e "${GREEN}Brew Leaves Installed!${COLOR_OFF}"
 else
-  echo "No brew leaves to install."
+  	echo -e "${YELLOW}No brew leaves to install!${COLOR_OFF}"
 fi
 
 # Install zap-zsh
 if [[ ! -f "$HOME/.local/share/zap/zap.zsh" ]]; then
     echo -e "${CYAN}Installing Zap-Zsh...${COLOR_OFF}"
     zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+  	echo -e "${GREEN}Zap-Zsh Installed!${COLOR_OFF}"
+fi
+
+# Install Mise
+if [[ ! -f "$HOME/.local/bin/mise" ]]; then
+    echo -e "${CYAN}Installing Mise...${COLOR_OFF}"
+		zsh $(curl https://mise.run | sh)
+  	echo -e "${GREEN}Mise Installed!${COLOR_OFF}"
 fi
 
 # NeoVim
-## Currently done through Brew above
+## Currently done through Mise
 
 # Stow
 ## Currently done through Brew above
@@ -49,7 +59,7 @@ if [[ ! -f "$HOME/.local/bin/lvim" ]]; then
   echo -e "${CYAN}Installing LunarVim...${COLOR_OFF}"
   ## Might need to check and install lvim deps like cargo, node, make, etc.
   ## Also need to remove the default lvim user config file and use stowed version.
-  LV_BRANCH='release-1.3/neovim-0.9' zsh <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+  LV_BRANCH='release-1.4/neovim-0.9' zsh <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
 fi
 
 # Tmux Plugin Manager
@@ -60,7 +70,6 @@ fi
 
 
 # Volta
-
 if [[ ! -f "$HOME/.volta/bin/volta" ]]; then
   echo -e "${YELLOW}Volta is not installed${COLOR_OFF}"
   echo -e -n "${YELLOW}Would you like to install it? ${GREEN}(y/n):${COLOR_OFF} "   
@@ -69,6 +78,7 @@ if [[ ! -f "$HOME/.volta/bin/volta" ]]; then
   if [[ $answer == "Y" || $answer == "y" ]]; then
     echo -e "${CYAN}Installing Volta...${COLOR_OFF}"
     zsh <(curl https://get.volta.sh)
+    echo -e "${GREEN}Volta Installed!${COLOR_OFF}"
   else
     echo -e "${CYAN}Volta will not be installed.${COLOR_OFF}"
   fi
--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -1,12 +1,48 @@
-# PATH
-export CARGO_BIN="$HOME/.cargo/bin"
-export VOLTA_BIN="$HOME/.volta/bin"
-export LOCAL_BIN="$HOME/.local/bin"
-export PULUMI_BIN="$HOME/.pulumi/bin"
-export PATH="$VOLTA_BIN:$CARGO_BIN:$LOCAL_BIN:$PULUMI_BIN:$PATH"
+function add_to_path() {
+	local bin_path="$1"
+	if [[ -d "${bin_path}" ]]; then
+      if [[ -z "${NEW_PATH}" ]]; then
+          NEW_PATH="${bin_path}"
+      else
+          NEW_PATH="${NEW_PATH}:${bin_path}"
+      fi
+  else
+      echo "Warning: Bin Path Directory Not Found: ${bin_path}" >&2
+  fi
+}
+
+BREW_BIN="/home/linuxbrew/.linuxbrew/bin"
+if [[ $(uname) == "Darwin" && $(uname -m) == "arm64" ]]; then
+	BREW_BIN="/opt/homebrew/bin"
+elif [[ $(uname) == "Darwin" && $(uname -m) == "x86_64" ]]; then
+	BREW_BIN="/usr/local/bin"
+else
+	BREW_BIN="/home/linuxbrew/.linuxbrew/bin"
+fi
+
+# Define Paths
+CARGO_BIN="$HOME/.cargo/bin"
+VOLTA_BIN="$HOME/.volta/bin"
+LOCAL_BIN="$HOME/.local/bin"
+PULUMI_BIN="$HOME/.pulumi/bin"
+
+# Build PATH
+NEW_PATH=""
+add_to_path "${LOCAL_BIN}"
+add_to_path "${BREW_BIN}"
+add_to_path "${CARGO_BIN}"
+add_to_path "${PULUMI_BIN}"
+add_to_path "${VOLTA_BIN}"
+
+# Export PATH
+if [[ -n "${NEW_PATH}" ]]; then
+	export PATH="$NEW_PATH:$PATH"
+fi
+	
+
 
 # EDITOR
-export EDITOR="lvim"
+export EDITOR="nvim"
 
 # FZF
 export FZF_DEFAULT_OPTS="--tmux 80% --layout=reverse --border"
