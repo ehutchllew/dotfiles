08d22043809168664a6249236b2c325fbb701c62
Evan <ehutch7@gmail.com>
2025-01-11 22:28:38 +0000

    Fixed kanagawa indent gradient

--- nvim/.config/nvim/lua/config/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/config/plugins/indent-blankline.lua
@@ -21,19 +21,21 @@ return {
 				"Grayscale4",
 				"Grayscale5",
 				"Grayscale6",
+				"Grayscale7",
 			}
 
 			local hooks = require("ibl.hooks")
 			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
 				-- NOTE: When changing colorscheme, update these to maintain a gradient-like indent effect.
 				-- KANAGAWA
-				vim.api.nvim_set_hl(0, "Grayscale0", { background = "#16161D" })
-				vim.api.nvim_set_hl(0, "Grayscale1", { background = "#1A1A21" })
-				vim.api.nvim_set_hl(0, "Grayscale2", { background = "#1E1E25" })
-				vim.api.nvim_set_hl(0, "Grayscale3", { background = "#222229" })
-				vim.api.nvim_set_hl(0, "Grayscale4", { background = "#26262D" })
-				vim.api.nvim_set_hl(0, "Grayscale5", { background = "#2A2A31" })
-				vim.api.nvim_set_hl(0, "Grayscale6", { background = "#2E2E35" })
+				vim.api.nvim_set_hl(0, "Grayscale0", { background = "#1F1F28" })
+				vim.api.nvim_set_hl(0, "Grayscale1", { background = "#1C1C25" })
+				vim.api.nvim_set_hl(0, "Grayscale2", { background = "#191922" })
+				vim.api.nvim_set_hl(0, "Grayscale3", { background = "#16161F" })
+				vim.api.nvim_set_hl(0, "Grayscale4", { background = "#13131C" })
+				vim.api.nvim_set_hl(0, "Grayscale5", { background = "#101009" })
+				vim.api.nvim_set_hl(0, "Grayscale6", { background = "#0E0E06" })
+				vim.api.nvim_set_hl(0, "Grayscale7", { background = "#0B0B03" })
 
 				-- ONEDARK
 				-- vim.api.nvim_set_hl(0, "Grayscale0", { background = "#1a212e" })
